8cd2869af20fff621c5ebf05ca5adceaf192f36a
James Ferrell <jamesferrell@Jamess-MacBook-Pro.local>
2020-12-10 17:29:04 -0500

    post request sending form data thru to server

--- src/screens/NewItem.js
+++ src/screens/NewItem.js
@@ -1,20 +1,32 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 
 const NewItem = () => {
+
+  const initialState = { 
+    name: '',
+    price: '',
+    category: '',
+    countInStock: '',
+    description: '',
+    renderCheck: false
+  }
+
+  const [eachEntry, setEachEntry] = useState(initialState);
+  const { name, price, category, countInStock, description, renderCheck } = eachEntry;
+  const handleInputChange = e => {
+    setEachEntry({ ...eachEntry, [e.target.name]: e.target.value})
+  }
+  console.log(eachEntry);
+
   const handleSubmit = useCallback(() => {
     console.log('new item submit');
     axios
-      .post('/api/add', { name: 'James' })
+      .post('/api/add', { eachEntry })
       .then(response => console.log(response.data))
       .catch(err => console.log(err.message));
-    // fetch('/api/add', {
-    //   method: 'POST',
-    //   body: JSON.stringify({ nickname: 'James' }),
-    //   headers: { 'Content-Type': 'application/json' },
-    // })
   })
 
   return (
@@ -23,16 +35,17 @@ const NewItem = () => {
       <Form>
         <Form.Group controlId="formBasicName">
           <Form.Label>Dish Name</Form.Label>
-          <Form.Control type="text" placeholder="Dish Name" />
+          <Form.Control name="name" type="text" placeholder="Dish Name" value={name} onChange={handleInputChange} />
         </Form.Group>
 
         <Form.Group controlId="formBasicPrice">
           <Form.Label>Price</Form.Label>
-          <Form.Control type="text" placeholder="Price" />
+          <Form.Control name="price" type="text" placeholder="Price" value={price} onChange={handleInputChange} />
         </Form.Group>
         <Form.Group controlId="exampleForm.ControlSelect1">
           <Form.Label>Category</Form.Label>
-          <Form.Control as="select">
+          <Form.Control name="category" value='' as="select" value={category} onChange={handleInputChange}>
+            <option key='blankChoice' hidden value />
             <option>Appetizers</option>
             <option>Salads</option>
             <option>Handhelds</option>
@@ -42,11 +55,11 @@ const NewItem = () => {
         </Form.Group>
         <Form.Group controlId="formBasicStock">
           <Form.Label>Number in Stock</Form.Label>
-          <Form.Control type="text" placeholder="Number in Stock" />
+          <Form.Control name="countInStock" type="text" placeholder="Number in Stock" value={countInStock} onChange={handleInputChange} />
         </Form.Group>
         <Form.Group controlId="formBasicDescription">
           <Form.Label>Description</Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="Description..." />
+          <Form.Control name="description" as="textarea" rows={3} placeholder="Description..." value={description} onChange={handleInputChange} />
         </Form.Group>
         <Form.Group controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Display Now" />
@@ -65,16 +78,3 @@ export default NewItem
 
 
 
-// const initialState = { 
-//   dishName: '',
-//   price: '',
-//   category: '',
-//   numberInStock: '',
-//   description: '',
-//   renderCheck: false
-// }
-// const [eachEntry, setEachEntry] = useState(initialState);
-
-// const handleInputChange = e => {
-//   setEachEntry({ ...eachEntry, [e.target.name]: e.target.value})
-// }
